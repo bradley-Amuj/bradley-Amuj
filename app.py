eb92cdab4cb409db007bc1c566618f22909cd858
Bradley Juma <bradleyjuma170@gmail.com>
2022-11-02 10:00:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,18 +6,23 @@ import os
 board = None
 
 def main():
-    endpoint_url = "https://lichess.org/api/puzzle/daily"
+    #endpoint_url = "https://lichess.org/api/puzzle/daily"
+    endpoint_url = "https://lichess.org/api/puzzle/Uk3YD"
+    
     response = requests.get(url=endpoint_url)
     data = response.json()
     pgn = data['game']['pgn']
     sln = data['puzzle']['solution']
     rating = data['puzzle']['rating']
     
+    print('This is the pgn'+ pgn)
+    print('This is the rating'+ str(rating))
+    print('This is the solution' + str(sln))
     
-    input_sln = os.environ['INPUT_SLN']
-    checkSln(input_sln)
-
     
+    # input_sln = os.environ['INPUT_SLN']
+    # checkSln(input_sln)
+ 
 def checkSln(sln):
     print(sln)
     
